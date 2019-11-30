125739a0404d19dd07644e73ffeb5eb173814ed7
joseleonardo1995 <leonardosabando03@hotmail.com>
2019-11-30 13:47:21 -0500

    subir

--- app/Http/Controllers/LoginSController.php
+++ app/Http/Controllers/LoginSController.php
@@ -23,33 +23,37 @@ class LoginSController extends Controller
           'base_uri' => $this->servidor.'Login',
         ]);
         $password= base64_encode($request->password);
-        $data = ['email'=>$request->email,'password'=>$password]; //EL REQUEST ES EL FORM DATA QUE VIENE EN EL AJAX
-        
-        $response = $client->request('POST','',['form_params' => $data]);
-               
-        $UsuariosLogin= json_decode((string) $response->getBody(), true);
 
-        if($UsuariosLogin !=null){
-            session_start();
-            $_SESSION['id']=$UsuariosLogin[0]['Id_Usuario'];
-            $_SESSION['nombre']=$UsuariosLogin[0]['Nombre'];
-            $_SESSION['apellido']=$UsuariosLogin[0]['Apellido'];
-            $_SESSION['cedula']=$UsuariosLogin[0]['Cedula'];
-            $_SESSION['celular']=$UsuariosLogin[0]['Celular'];
-            $_SESSION['direccion']=$UsuariosLogin[0]['Direccion'];
-            $_SESSION['email']=$UsuariosLogin[0]['email'];
-            $_SESSION['sexo']=$UsuariosLogin[0]['Sexo'];
-            $_SESSION['Id_tipo_Usuarios']=$UsuariosLogin[0]['Id_tipo_Usuarios'];
-            $_SESSION['Instagram']=$UsuariosLogin[0]['Instagram'];
-            $_SESSION['Facebook']=$UsuariosLogin[0]['Facebook'];
-            $_SESSION['Twitter']=$UsuariosLogin[0]['Twitter'];
-            $_SESSION['Intereses']=$UsuariosLogin[0]['Intereses'];
-            $_SESSION['Fecha_Nacimiento']=$UsuariosLogin[0]['Fecha_Nacimiento'];
-            $_SESSION['Password']=$UsuariosLogin[0]['Password'];
-            return redirect('/');
+        if($request->email != null && $request->password != null ){
+            $data = ['email'=>$request->email,'password'=>$password]; 
+            $response = $client->request('POST','',['form_params' => $data]);     
+            $UsuariosLogin= json_decode((string) $response->getBody(), true);
+            if($UsuariosLogin !=null){
+                session_start();
+                $_SESSION['id']=$UsuariosLogin[0]['Id_Usuario'];
+                $_SESSION['nombre']=$UsuariosLogin[0]['Nombre'];
+                $_SESSION['apellido']=$UsuariosLogin[0]['Apellido'];
+                $_SESSION['cedula']=$UsuariosLogin[0]['Cedula'];
+                $_SESSION['celular']=$UsuariosLogin[0]['Celular'];
+                $_SESSION['direccion']=$UsuariosLogin[0]['Direccion'];
+                $_SESSION['email']=$UsuariosLogin[0]['email'];
+                $_SESSION['sexo']=$UsuariosLogin[0]['Sexo'];
+                $_SESSION['Id_tipo_Usuarios']=$UsuariosLogin[0]['Id_tipo_Usuarios'];
+                $_SESSION['Instagram']=$UsuariosLogin[0]['Instagram'];
+                $_SESSION['Facebook']=$UsuariosLogin[0]['Facebook'];
+                $_SESSION['Twitter']=$UsuariosLogin[0]['Twitter'];
+                $_SESSION['Intereses']=$UsuariosLogin[0]['Intereses'];
+                $_SESSION['Fecha_Nacimiento']=$UsuariosLogin[0]['Fecha_Nacimiento'];
+                $_SESSION['Password']=$UsuariosLogin[0]['Password'];
+                return redirect('/');
+            }else{
+                return back()->withInput();
+            }
         }else{
-            return back()->withInput();
+             abort(404);
         }
+
+
     }
 
     //CERRAR SESION
